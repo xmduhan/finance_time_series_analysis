1f7fbd51952b7bcd7d67b36e391b037d0c06174f
xmduhan <xmduhan@gmail.com>
2017-08-29 17:53:25 +0800

    ...

--- test.R
+++ test.R
@@ -1,6 +1,11 @@
 
-da = read.table("data/m-ibm3dx2608.txt")
+# load data
+da = read.table("data/m-ibm3dx2608.txt", header=T)
 sibm = da[,2]
-Box.test(sibm)
-
 libm = log(sibm+1)
+
+#  
+Box.test(sibm, lag=5, type="Ljung-Box")
+
+# 
+Box.test(libm, lag=5, type="Ljung-Box")
